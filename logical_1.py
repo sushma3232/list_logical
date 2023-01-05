f056a2a26b98ccff8768334e4c991198152a1658
sushma3232 <121096745+sushma3232@users.noreply.github.com>
2023-01-05 14:51:45 +0530

    Add files via upload

---
+++ logical_1.py
@@ -0,0 +1,24 @@
+a="abhilash"
+i=0
+b=[]
+while i<len(a):
+    c=a[i].capitalize()
+    b.append(c)
+    i=i+1
+print(b)
+        
+        
+        
+        
+        
+a="AbhILasH"
+i=0
+b=[]
+while i<len(a):
+    if a[i].isupper():
+        b.append(i)
+    i=i+1
+print(b)
+        
+        
+        
